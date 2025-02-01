dd77ad3893e660b841066f770169f08a5e1eabf0
JeremDePass <depassorio.jeremy@gmail.com>
2025-02-01 12:55:39 -0500

    dropzone-component file

--- quizapp/src/app/page.js
+++ quizapp/src/app/page.js
@@ -1,9 +1,11 @@
 
 
 export default function Home() {
-  return (
-    <main className="flex justify-center items-center flex-col min-h-screen px-6 mx-auto">
-      <h1>Hello, I'm Kira!</h1>
-    </main>
-  );
-}
+    return (
+      <main className="flex justify-center items-center flex-col min-h-screen px-6 mx-auto">
+        <h1>Hello, I'm Kira!</h1>
+      </main>
+      
+    );
+  }
+  
