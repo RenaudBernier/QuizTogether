e1141c623d8649a7c8d92340b627195e51b2d6d0
faydum516 <77711700+faydum516@users.noreply.github.com>
2025-02-01 14:15:58 -0500

    second commit

--- quizapp/src/app/page.js
+++ quizapp/src/app/page.js
@@ -1,11 +1,7 @@
-
-
 export default function Home() {
     return (
       <main className="flex justify-center items-center flex-col min-h-screen px-6 mx-auto">
-        <h1>Hello, I'm Kira!</h1>
       </main>
-      
     );
   }
   
