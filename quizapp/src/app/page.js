90258b587ca58e65cdfeecb4f502e5163406fffb
faydum516 <77711700+faydum516@users.noreply.github.com>
2025-02-01 20:15:46 -0500

    fifth commit

--- quizapp/src/app/page.js
+++ quizapp/src/app/page.js
@@ -1,14 +1,25 @@
+import Link from 'next/link';
+import NoteUploader from '../../components/NoteUploader';
+
 export default function Home() {
     return (
-<main className="flex justify-center items-center flex-col h-screen px-6 mx-auto">
-  <h1 className="text-4xl font-bold text-center text-black dark:text-white">
-    Welcome to QuizMaster 🚀
-  </h1>
-  <p className="text-lg text-center text-neutral-500 dark:text-neutral-400">
-    The ultimate quiz app for developers
-  </p>
-</main>
-
+        <main className="flex flex-col justify-center items-center min-h-screen text-center pt-16 mx-auto px-6">
+          <h1 className="text-4xl font-bold text-black dark:text-white">
+            Welcome to QuizMaster 🚀
+          </h1>
+          <p className="text-lg text-neutral-500 dark:text-neutral-400 mb-6 py-3">
+            The ultimate quiz app for developers
+          </p>
+          <div className="flex space-x-4">
+            <Link href="/login" className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700">
+                Login
+            </Link>
+            <Link href="/signup" className="bg-yellow-400 text-black px-6 py-3 rounded-lg hover:bg-yellow-500">
+                Sign Up
+            </Link>
+          </div>
+          <NoteUploader />
+        </main>
     );
   }
   
