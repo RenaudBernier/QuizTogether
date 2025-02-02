3d7868bfe28cc2a80b43738d8ce70c942bfc6006
faydum516 <77711700+faydum516@users.noreply.github.com>
2025-02-01 22:13:19 -0500

    seventh commit

--- quizapp/components/Welcome.jsx
+++ quizapp/components/Welcome.jsx
@@ -4,14 +4,14 @@ import Link from 'next/link';
 
 const Welcome = () => {
   return (
-    <>
+    <section className="flex flex-col">
         <h1 className="text-4xl font-bold text-black dark:text-white">
             Welcome to QuizMaster 🚀
         </h1>
         <p className="text-lg text-neutral-500 dark:text-neutral-400 mb-6 py-3">
             The ultimate quiz app for developers
         </p>
-        <div className="flex space-x-4">
+        <div className="flex justify-center space-x-4">
             <Link href="/login" className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700">
                 Login
             </Link>
@@ -19,7 +19,7 @@ const Welcome = () => {
                 Sign Up
             </Link>
         </div>
-    </>
+    </section>
   )
 }
 
--- quizapp/src/app/page.js
+++ quizapp/src/app/page.js
@@ -1,9 +1,11 @@
 import Welcome from '../../components/Welcome';
+import UploadForm from '../../components/dropzone component';
 
 export default function Home() {
     return (
-        <main className="flex flex-col justify-center items-center min-h-screen text-center pt-16 mx-auto px-6">
+        <main className="flex flex-col justify-center gap-[40px] items-center min-h-screen text-center pt-16 mx-auto px-6">
           <Welcome />
+          <UploadForm />
         </main>
     );
   }
