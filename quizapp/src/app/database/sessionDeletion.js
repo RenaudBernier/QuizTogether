9ec70e1ec91436a369e0dede216c61e425ad1d78
blanche <79345528+poewt@users.noreply.github.com>
2025-02-02 02:16:21 -0500

    Add session deletion function

---
+++ quizapp/src/app/database/sessionDeletion.js
@@ -0,0 +1,16 @@
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../firebase";
+
+export async function deleteSession(sessionId) {
+    try {
+        // Delete session from Firestore
+        await deleteDoc(doc(db, "sessions", sessionId));
+        console.log("Successfully deleted session.");
+    } catch (error) {
+        console.error("Could not delete session from Firestore.");
+    } finally {
+        // Delete session stored in local/sessionStorage
+        // regardless of what happens
+        sessionStorage.removeItem("quiz-session-id"); 
+    }
+}
