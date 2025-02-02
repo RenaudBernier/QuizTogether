5cc63586732c9c184ac09857e97b45f11647258c
blanche <79345528+poewt@users.noreply.github.com>
2025-02-02 09:24:56 -0500

    Update hero banner sizing

--- quizapp/src/app/components/landing-page/HeroBanner.jsx
+++ quizapp/src/app/components/landing-page/HeroBanner.jsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function HeroBanner() {
     return (
-        <div className=" bg-blue-100 w-full h-full relative">
-            <Image src={"/hero_banner.png"} width={1920} height={1080} alt="Hero Banner"/>
+        <div className=" bg-blue-100 w-full h-[400px] relative">
+            <Image src={"/hero_banner.png"} objectFit="cover" layout="fill" alt="Hero Banner"/>
             <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-xl">
                 {/* <h1 className="uppercase font-bold text-[42px]">QuizIt</h1> */}
             </div>
